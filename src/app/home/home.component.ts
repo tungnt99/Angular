edec327a09fd6cd30d5485f5478b1b9bc4901b8e
tungnt99 <thanhtung99yl@gmail.com>
2022-01-28 14:04:15 +0700

    ngIf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public name = 'Thanh Tùng';
+  public age = 25;
   constructor() { }
 
   ngOnInit(): void {
